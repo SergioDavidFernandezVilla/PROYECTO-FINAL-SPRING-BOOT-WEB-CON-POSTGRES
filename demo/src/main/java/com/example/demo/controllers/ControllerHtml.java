dab2550ead0743464c85218d7a75a249f912eaf1
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2024-12-17 00:06:38 +0000

    "fix errors logic"

--- demo/src/main/java/com/example/demo/controllers/ControllerHtml.java
+++ demo/src/main/java/com/example/demo/controllers/ControllerHtml.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.models.Anuncio;
 import com.example.demo.services.ServicesAnuncio;
@@ -15,19 +17,18 @@ public class ControllerHtml {
     @Autowired
     private ServicesAnuncio servicesAnuncio;
 
-    @GetMapping("/")
-    public String WelcomePage(Model model) {
-        model.addAttribute("mensaje", "Bienvenidos al tablón de anuncios");
-        model.addAttribute("anuncios", servicesAnuncio.GetListAnuncios()); // Mostrar lista de anuncios
-        return "index"; // Cargar el template index.html
+    @GetMapping("/formulario-anuncio")
+    public String showForm(Model model) {
+        model.addAttribute("anuncio", new Anuncio());
+        return "new-anuncio"; // Nombre de tu archivo HTML
     }
 
-    // Mostrar el formulario
-    @GetMapping("/anuncio/crear-anuncio")
-    public String mostrarFormulario(Model model) {
-        model.addAttribute("anuncio", new Anuncio()); // Crear un objeto vacío para el formulario
-        return "new-anuncio"; // Nombre del template HTML
+    @PostMapping
+    public String createAnuncio(@ModelAttribute Anuncio anuncio){
+        servicesAnuncio.CreateAnuncio(anuncio);
+        return "redirect:/anuncios"; // Redirige a la lista de anuncios
     }
+
 
     @GetMapping("/insert")
     public String InsertMessage(Model model){
