d5b0276855fc19240a34f28d1aa2ebfb114c79de
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2024-12-17 04:13:46 +0000

    "fix error service"

--- demo/src/main/java/com/example/demo/Details/CustomUserDetailsService.java
+++ demo/src/main/java/com/example/demo/Details/CustomUserDetailsService.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.config.CustomUserDetails;
 import com.example.demo.models.User;
 import com.example.demo.repository.RepositoryUser;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private RepositoryUser repositoryUser;
