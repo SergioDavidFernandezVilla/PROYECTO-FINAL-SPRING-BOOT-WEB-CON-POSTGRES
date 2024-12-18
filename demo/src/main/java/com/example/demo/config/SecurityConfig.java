a82284da8f9d098afd7274692399ac22122bb62f
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2024-12-18 07:23:14 +0000

    "fix erros"

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
                     // RUTA ANUNCIO
                     http.requestMatchers(HttpMethod.GET, "/anuncio").hasAnyRole("ADMIN", "DEVELOPER", "USER");
                     http.requestMatchers(HttpMethod.POST, "/anuncio").hasAnyRole("ADMIN", "DEVELOPER");
-
+                    http.requestMatchers(HttpMethod.GET, "/anuncio/{id}").hasAnyRole("ADMIN", "DEVELOPER", "USER");
                     
                     http.requestMatchers(HttpMethod.GET, "/new").hasAnyRole("ADMIN", "DEVELOPER", "USER");
 
--- demo/src/main/java/com/example/demo/controllers/ControllerAnuncio.java
+++ demo/src/main/java/com/example/demo/controllers/ControllerAnuncio.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,7 +52,6 @@ public class ControllerAnuncio {
 
     // METODO GET POR ID
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ADMIN', 'DEVELOPER', 'USER')")
     public Anuncio GetAnuncioById(@PathVariable Long id){
         return servicesAnuncio.GetAnuncioById(id);
     }
