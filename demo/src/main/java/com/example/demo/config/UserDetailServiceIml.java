0d7660aaf9c69fbc2da1600f138ae6d7ecd457db
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2024-12-18 05:00:39 +0000

    "fix error service"

--- demo/src/main/java/com/example/demo/config/UserDetailServiceIml.java
+++ demo/src/main/java/com/example/demo/config/UserDetailServiceIml.java
@@ -9,10 +9,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.models.UserEntity;
 import com.example.demo.repository.RepositoryUser;
 
+@Service
 public class UserDetailServiceIml implements UserDetailsService{
 
     @Autowired
