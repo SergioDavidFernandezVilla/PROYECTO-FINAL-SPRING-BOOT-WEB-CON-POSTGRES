e3e703985a471e92e6155ccc5063ab8aa93a0fae
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2024-12-18 03:45:25 +0000

    "add files auth"

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.example.demo.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -21,10 +31,26 @@ public class SecurityConfig {
     @Autowired
     AuthenticationConfiguration authenticationConfiguration;
     
-    @Bean
+    // METODO SECURITY FILTER CHAIN
+   /*  @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity.build();
-    }
+        return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .httpBasic(Customizer.withDefaults())
+        .sessionManagement(sesion -> sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(http -> {
+            // CONFIGURAR ENDPOINTS PUBLICOS
+            http.requestMatchers(HttpMethod.GET, "/auth/get").permitAll();
+
+            // CONFIGURAR ENDPOINTS PRIVADOS
+            http.requestMatchers(HttpMethod.POST, "/auth/post").hasAnyRole("ADMIN", "DEVELOPER");
+            http.requestMatchers(HttpMethod.PATCH, "/auth/patch").hasAnyAuthority("REFACTOR");
+
+            // CONFIGURAR EL RESTO ENDPOINTS POR DEFECTO
+            http.anyRequest().denyAll();
+        })
+        .build();
+    } */
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
@@ -34,9 +60,32 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(null);
-        provider.setUserDetailsService(null);
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
         return provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        List<UserDetails> users = new ArrayList<>();
+
+        users.add(
+            User.withUsername("user")
+            .password("password")
+            .roles("USER")
+            .authorities("READ", "CREATE")
+            .build());
+
+        users.add(
+            User.withUsername("admin")
+            .password("password")
+            .roles("ADMIN")
+            .authorities("READ", "CREATE", "UPDATE", "DELETE")
+            .build());
+        
+        return new InMemoryUserDetailsManager(
+            users
+        );
     }
 
     @Bean
---
+++ demo/src/main/java/com/example/demo/controllers/ControllerIndex.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@PreAuthorize("denyAll()")
+public class ControllerIndex {
+    
+
+    @GetMapping("/")
+    @PreAuthorize("permitAll()")
+    public String index(){
+        return "Hola Mundo AUTH - VISTA NO PROTEGIDA";
+    }
+
+    @GetMapping("/protected")
+    @PreAuthorize("hasAuthority('READ')")
+    public String ProtectedView(){
+        return "Hola Mundo AUTH - VISTA PROTEGIDA";
+    }
+
+}
